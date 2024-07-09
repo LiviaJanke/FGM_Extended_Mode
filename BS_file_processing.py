8d1c7d6257b09b1374ebbbd8f5fb41d67381339e
Livia <eichmliv12@gmail,com>
2024-07-09 14:44:26 +0100

    BS_decode now takes BS file, filters out only BM3 dumps and returns valid decoded data

--- BS_file_processing.py
+++ BS_file_processing.py
@@ -22,9 +22,13 @@ from datetime import datetime,timedelta
 from scipy.stats import linregress
 import sys
 
+# ID definitions
+
 validphid=(0x1F,0x47,0x6F,0x97,0x26,0x4E,0x76,0x9E,0x2D,0x55,0x7D,0xA5)
 sciphid=(0x1F,0x47,0x6F,0x97,0x26,0x4E,0x76,0x9E)
 fgmhkphid=(0x2D,0x55,0x7D,0xA5)
+
+# Classes
 
 class packet():
 
@@ -60,11 +64,212 @@ class packet():
     
     def __str__(self):
         return("{:7s}".format("#"+str(self.pktcnt))+" | "+" ".join('{:02X}'.format(n) for n in self.cdds)+" | "+" ".join('{:02X}'.format(n) for n in self.payload[0:30]))
+
+
+
     
+    
+
+class odd_ext_packet():
+    
+    def __init__(self,d):
+        
+        self.pcktheader = d[0:34]
+        
+        self.prev = d[34:42]
+        
+        self.vectors = d[42:3590]
+        
+        self.end_str = d[3590:]
+    
+    
+    def __str__(self):
+        
+        return('odd packet')        
+        
+
+
+
+
+class even_ext_packet():
+    
+    def __init__(self,d):
+        
+        self.pcktheader = d[0:34]
+        
+        self.vectors = d[34:3586]
+        
+        self.partial = d[3586:3590]
+        
+        self.end_str = d[3590:].hex()
+        
+        self.end_vals=int.from_bytes(d[3590:],"big")
+        
+    
+    def __str__(self):
+        
+        return('even packet')
+    
+class ext_vector():
+    
+    def __init__(self,d):
+        
+        self.x = int.from_bytes(d[0:2], 'big')
+
+        self.y = int.from_bytes(d[2:4], 'big')
+        
+        self.z = int.from_bytes(d[4:6], 'big')
+        
+        self.range = str(d[6])[0]
+        
+        self.reset = str(d[6])[1] + str(d[7])
+    
+    def __str__(self):
+        return(self.x, self.y, self.z, self.range, self.reset)
+
+
+# Functions
+
+def s16(val):
+    
+    value = int(val)
+    
+    return -(value & 0x8000) | (value & 0x7fff)
+
+def packet_decoding_even(number):
+    
+    packig = ext_bytes[number][68:7180]
+    
+    x_vals = []
+    
+    y_vals = []
+    
+    z_vals = []
+    
+    range_vals = []
+    
+    reset_vals = []
+    
+    
+    for i in np.arange(0,len(packig), 16):
+        
+        byte_num = i/2
+        
+        if byte_num < 3552:
+            
+            x = s16(int(packig[i:i+4],16))
+            
+            y = s16(int(packig[i+4:i+8],16))
+            
+            z = s16(int(packig[i+8:i+12],16))
+            
+            range_val = s16(int(packig[i+12],16))
+            
+            reset_val = s16(int(packig[i+13:i+16],16))
+            
+        else:
+
+            x= s16(int(packig[i:i+4],16))
+            
+            y = s16(int(packig[i+4:i+8],16))
+
+            z = 'af'
+            
+            range_val = 'af'
+            
+            reset_val = 'af'
+            
+        
+        x_vals.append(x)
+        
+        y_vals.append(y)
+        
+        z_vals.append(z)
+        
+        range_vals.append(range_val)
+        
+        reset_vals.append(reset_val)
+        
+    
+    df_p = pd.DataFrame(zip(reset_vals, range_vals, x_vals, y_vals, z_vals))
+    
+    df_p.columns = ['reset', 'resolution', 'x', 'y', 'z']    
+         
+    return df_p
+
+
+def packet_decoding_odd(number):
+    
+    packig = ext_bytes[number][68:7180]
+    
+    x_vals = []
+    
+    y_vals = []
+    
+    z_vals = []
+    
+    range_vals = []
+    
+    reset_vals = []
+    
+    
+    for i in np.arange(0, len(packig) - 16, 16):
+        
+        byte_num = i/2
+        
+        if byte_num > 4:
+        
+            x = s16(int(packig[i:i+4],16))
+            
+            y = s16(int(packig[i+4:i+8],16))
+            
+            z = s16(int(packig[i+8:i+12],16))
+            
+            range_val = s16(int(packig[i+12],16))
+            
+            reset_val = s16(int(packig[i+13:i+16],16))
+            
+        else:
+            
+            x = 'bef'
+            
+            y = 'bef'
+            
+            z = s16(int(packig[i:i+4],16))
+            
+            range_val = s16(int(packig[i+5],16))
+            
+            reset_val = s16(int(packig[i+6:i+8],16))
+            
+
+    
+        x_vals.append(x)
+        
+        y_vals.append(y)
+        
+        z_vals.append(z)
+        
+        range_vals.append(range_val)
+        
+        reset_vals.append(reset_val)
+        
+    
+    df_p = pd.DataFrame(zip(reset_vals, range_vals, x_vals, y_vals, z_vals))
+    
+    df_p.columns = ['reset', 'resolution', 'x', 'y', 'z']    
+         
+    return df_p
+
+
+
     
 #%%
 
-file = open('C1_020227_B.BS',"rb")
+craft_dumpdate = 'C1_020227_B'
+
+BS_filename = craft_dumpdate +'.BS'
+
+file = open(BS_filename,"rb")
 
 # this is the file dumped on 020227
 # contains data from 2002-02-27T01:19:54.000Z Entry to 2002-02-27T08:25:54.000Z Exit
@@ -96,37 +301,11 @@ while True:
         break
     
 
-i=0
-start=0
-while i<(len(packets)-1):
-    if (packets[i+1].reset)<(packets[i].reset):
-        resets=[p.reset for p in packets[start:i+1]]
-        listmicros=[p.micros for p in packets[start:i+1]]
-        slope,intercept,rvalue, pvalue,stderr=linregress(resets,listmicros)
-        regressionvalues=[p.reset*slope+intercept for p in packets[start:i+1]]
-
-        print(min(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),max(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),sep=" ",end=" ")
-        print("{:04X}".format(min(p.reset for p in packets[start:i+1])),"{:04X}".format(max(p.reset for p in packets[start:i+1])),end=" ")
-        print("{:23.15f}".format(slope),"{:19.2f}".format(intercept))
-        start=i+1
-    i+=1
-    
 
 
-resets=[p.reset for p in packets[start:i+1]]
-size=[p.size for p in packets[start:i+1]]
-listmicros=[p.micros for p in packets[start:i+1]]
-slope,intercept,rvalue, pvalue,stderr=linregress(resets,listmicros)
-regressionvalues=[p.reset*slope+intercept for p in packets[start:i+1]]    
-contents = [p.payload for p in packets[start:i+1]]
-status = [p.status for p in packets[start:i+1]]
+#%%
 
-counts = [p.pktcnt for p in packets[start:i+1]]
-
-
-print(min(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),max(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),sep=" ",end=" ")
-print("{:04X}".format(min(p.reset for p in packets[start:i+1])),"{:04X}".format(max(p.reset for p in packets[start:i+1])),end=" ")
-print("{:23.15f}".format(slope),"{:19.2f}".format(intercept))
+del data
 
 #%%
 
@@ -160,132 +339,14 @@ for i in ext_packets:
     ext_bytes.append(hex_vals)
 
 
+
+
 #%%
 
 num_of_packets = len(ext_bytes) 
 
 packet_range = np.arange(0, num_of_packets)
 
-#%%
-
-def packet_decoding_even(number):
-    
-    packig = ext_bytes[number]
-    
-    x_vals = []
-    
-    y_vals = []
-    
-    z_vals = []
-    
-    range_vals = []
-    
-    reset_vals = []
-    
-    
-    for i in np.arange(34, 3590, 8):
-        
-        if i < 3601:
-            
-            x = packig[i:i+2]
-            
-            y = packig[i+2:i+4]
-            
-            z = packig[i+4:i+6]
-            
-            range_val = packig[i+6][0]
-            
-            reset_val = packig[i+6][0] + packig[i+7]
-            
-        else:
-
-            x= packig[i:i+2]
-            
-            y = packig[i+2:i+4]
-
-            z = 'af'
-            
-            range_val = 'af'
-            
-            reset_val = 'af'
-            
-        
-        x_vals.append(x)
-        
-        y_vals.append(y)
-        
-        z_vals.append(z)
-        
-        range_vals.append(range_val)
-        
-        reset_vals.append(reset_val)
-        
-    
-    df_p = pd.DataFrame(zip(reset_vals, range_vals, x_vals, y_vals, z_vals))
-    
-    df_p.columns = ['reset', 'resolution', 'x', 'y', 'z']    
-         
-    return df_p
-
-
-def packet_decoding_odd(number):
-    
-    packig = ext_bytes[number]
-    
-    x_vals = []
-    
-    y_vals = []
-    
-    z_vals = []
-    
-    range_vals = []
-    
-    reset_vals = []
-    
-    
-    for i in np.arange(34, 3590, 8):
-        
-        if i > 41:
-        
-            x= packig[i:i+2]
-            
-            y = packig[i+2:i+4]
-            
-            z = packig[i+4:i+6]
-            
-            range_val = packig[i+6][0]
-            
-            reset_val = packig[i+6][0] + packig[i+7]
-            
-        else:
-            
-            x = 'bef'
-            
-            y = 'bef'
-            
-            z = packig[i+4:i+6]
-            
-            range_val = packig[i+6][0]
-            
-            reset_val = packig[i+6][0] + packig[i+7]
-
-    
-        x_vals.append(x)
-        
-        y_vals.append(y)
-        
-        z_vals.append(z)
-        
-        range_vals.append(range_val)
-        
-        reset_vals.append(reset_val)
-        
-    
-    df_p = pd.DataFrame(zip(reset_vals, range_vals, x_vals, y_vals, z_vals))
-    
-    df_p.columns = ['reset', 'resolution', 'x', 'y', 'z']    
-         
-    return df_p
 
 
 #%%
@@ -304,25 +365,25 @@ all_valid_dfs = []
 
 for i in packet_range:
     
-    even_df_i = packet_decoding_even(int(i))
+    even_df_i = packet_decoding_even(i)
     
     even_df_i['reset'] = even_df_i['reset'].astype(str)
     
     ecount, eunique, etop, efreq = even_df_i['reset'].describe()
     
-    odd_df_i = packet_decoding_odd(int(i))
+    odd_df_i = packet_decoding_odd(i)
     
     odd_df_i['reset'] = odd_df_i['reset'].astype(str)
     
     ocount, ounique, otop, ofreq = odd_df_i['reset'].describe()
     
-    if eunique < 100:
+    if eunique < 60:
         
         all_valid_dfs.append(even_df_i)
         
         valid_nums_even_decoded.append(i)
         
-    elif ounique < 100:
+    elif ounique <60:
         
         all_valid_dfs.append(odd_df_i)
         
@@ -335,10 +396,121 @@ for i in packet_range:
 #%%    
     
 all_valid_df = pd.concat(all_valid_dfs)
+
+del all_valid_dfs
+
     
+#%%
+
+sequential_data = all_valid_df.reset_index(drop = True)
+
+del all_valid_df
+
+
+
+#%%
+
+bef_indices = sequential_data.loc[sequential_data['x'] == 'bef'].index.tolist()
+
+af_indices = sequential_data.loc[sequential_data['z'] == 'af'].index.tolist()
+
+
+#%%
+
+for i in af_indices:
     
+    if i <  len(sequential_data['reset']) - 1:
     
+        sequential_data.loc[i,'reset'] = sequential_data.loc[i+1, 'reset']
     
+        sequential_data.loc[i,'resolution'] = sequential_data.loc[i+1, 'resolution']
+    
+        sequential_data.loc[i,'z'] = sequential_data.loc[i+1,'z']
+    
+    else:
+        
+        bef_indices.append(i)
+
+    
+
+#%%
+
+
+df_complete_indexed = sequential_data.drop(labels = bef_indices, axis = 0)
+
+del sequential_data
+
+#%%
+
+df_complete_indexed['reset'] = df_complete_indexed['reset'].astype(float)
+
+df_complete_indexed['resolution'] = df_complete_indexed['resolution'].astype(float)
+
+df_complete_indexed['x'] = df_complete_indexed['x'].astype(float)
+
+df_complete_indexed['y'] = df_complete_indexed['y'].astype(float)
+
+df_complete_indexed['z'] = df_complete_indexed['z'].astype(float)
+
+#%%
+
+reset_vecs = df_complete_indexed['reset']
+
+plt.plot(reset_vecs, linewidth = 0, marker = '.')
+
+
+#%%
+
+df_complete = df_complete_indexed.reset_index(drop = True)
+
+
+#%%
+
+filebase = 'C:\FGM_Extended_Mode\BS_decoded_files'
+
+
+
+ext = '.csv'
+
+filepath = filebase +'/' + craft_dumpdate + '_clean_decode' + ext
+
+df_complete.to_csv(filepath)
+
+#%%
+
+#timestamping code stuff
+
+i=0
+start=0
+while i<(len(packets)-1):
+    if (packets[i+1].reset)<(packets[i].reset):
+        resets=[p.reset for p in packets[start:i+1]]
+        listmicros=[p.micros for p in packets[start:i+1]]
+        slope,intercept,rvalue, pvalue,stderr=linregress(resets,listmicros)
+        regressionvalues=[p.reset*slope+intercept for p in packets[start:i+1]]
+
+        print(min(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),max(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),sep=" ",end=" ")
+        print("{:04X}".format(min(p.reset for p in packets[start:i+1])),"{:04X}".format(max(p.reset for p in packets[start:i+1])),end=" ")
+        print("{:23.15f}".format(slope),"{:19.2f}".format(intercept))
+        start=i+1
+    i+=1
+    
+
+
+resets=[p.reset for p in packets[start:i+1]]
+size=[p.size for p in packets[start:i+1]]
+listmicros=[p.micros for p in packets[start:i+1]]
+slope,intercept,rvalue, pvalue,stderr=linregress(resets,listmicros)
+regressionvalues=[p.reset*slope+intercept for p in packets[start:i+1]]    
+contents = [p.payload for p in packets[start:i+1]]
+status = [p.status for p in packets[start:i+1]]
+
+counts = [p.pktcnt for p in packets[start:i+1]]
+
+
+print(min(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),max(p.scet for p in packets[start:i+1]).strftime("%Y-%m-%dT%H:%M:%SZ"),sep=" ",end=" ")
+print("{:04X}".format(min(p.reset for p in packets[start:i+1])),"{:04X}".format(max(p.reset for p in packets[start:i+1])),end=" ")
+print("{:23.15f}".format(slope),"{:19.2f}".format(intercept))
     
     
     
