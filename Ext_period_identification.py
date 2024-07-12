ef0f1d08d578b860d91997463e3a6f56495893db
Livia <eichmliv12@gmail,com>
2024-07-12 16:42:14 +0100

    trying to match up entries and exits (unsuccesfully)

--- Ext_period_identification.py
+++ Ext_period_identification.py
@@ -37,26 +37,80 @@ filebase = 'C:/FGM_Extended_Mode/SCCH_strings'
 craft = 'C1'
 
 
-filepath = filebase +'/' + craft + '/'
+filepath = filebase +'/' + craft 
 
-df_with_pos = pd.read_csv(filepath,usecols = range(5), delimiter = ' ', names = [])
+C1_Ext_entries = pd.read_csv(filepath + '_Ext_Entries',usecols = range(8), delimiter = ' ', names = ['Execution_Time', 'Uplink_Time', 'Command_Mnemonic', 'Command_Sequence_Name', 'Cat', 'Src', 'GS', 'Ver'])
 
+C1_Ext_entries_unique = C1_Ext_entries.drop_duplicates(subset = 'Execution_Time', keep = 'first').reset_index(drop = True)
 
+C1_Ext_exits = pd.read_csv(filepath + '_Ext_Exits',usecols = range(8), delimiter = ' ', names = ['Execution_Time', 'Uplink_Time', 'Command_Mnemonic', 'Command_Sequence_Name', 'Cat', 'Src', 'GS', 'Ver'])
+
+C1_Ext_exits_unique = C1_Ext_exits.drop_duplicates(subset = 'Execution_Time', keep = 'first').reset_index(drop = True)
+
+C1_MSA_dumps = pd.read_csv(filepath + '_MSA_Dumps',usecols = range(8), delimiter = ' ', names = ['Execution_Time', 'Uplink_Time', 'Command_Mnemonic', 'Command_Sequence_Name', 'Cat', 'Src', 'GS', 'Ver'])
+
+C1_MSA_dumps_unique = C1_MSA_dumps.drop_duplicates(subset = 'Execution_Time', keep = 'first').reset_index(drop = True)
 
 #%%
 
 
+MSA_dump_times = pd.to_datetime(C1_MSA_dumps_unique['Execution_Time'].str[:-1])
+
+Ext_entry_times = pd.to_datetime(C1_Ext_entries_unique['Execution_Time'].str[:-1])
+
+Ext_exit_times = pd.to_datetime(C1_Ext_exits_unique['Execution_Time'].str[:-1])
 
 
+#%%
+
+# Want to pair up entry, exit and MSA dump times
+
+entries = []
+
+exits = []
+
+dumps = []
+
+unmatched_entries = []
+
+unmatched_exits = []
+
+unmatched_exit_counter = 0
+
+for i in np.arange(0, 10):#len(Ext_entry_times) - 1):
+    
+    entry = Ext_entry_times[i]
+    
+    print(entry)
+    
+    next_entry = Ext_entry_times[i + 1]
+    
+    print(next_entry)
+    
+    exit_time =  Ext_exit_times[i + unmatched_exit_counter]
+    
+    print(exit_time)
+        
+    if exit_time < next_entry:
+        
+        if exit_time > entry and exit_time < next_entry :
+        
+            entries.append(entry)
+            exits.append(exit_time)
+            
+        else:
+            
+            
+        
+    else:
+        
+        unmatched_exit_counter +=1
+        
+        
+    
 
 
-
-
-
-
-
-
-
+    
 
 
 
